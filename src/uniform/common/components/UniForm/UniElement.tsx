fcbe9b8d19468ab43924242c33e2d5bcf6e2e9b0
Ciyin Huang <ciyinhuang@gmail.com>
2020-04-14 12:18:44 +0800

    up

--- src/uniform/common/components/UniForm/UniElement.tsx
+++ src/uniform/common/components/UniForm/UniElement.tsx
@@ -3,8 +3,9 @@ import { View, Text, Block } from "@tarojs/components";
 import { observer } from "mobx-react";
 import Component, { BaseComponentPropsType } from "../BaseComponent";
 import SchemaListItem from "../schema/SchemaListItem";
+import SchemaTaroUI from "../schema/SchemaTaroUI";
 import { UniElementStore } from "../../stores/UniElementStore";
-import SchemaList from "../schema/SchemaList";
+import { isTaroUI } from '../../const'
 
 interface PageStateProps extends BaseComponentPropsType {
   path: string;
@@ -46,21 +47,18 @@ class UniElement extends Component<PageStateProps, any> {
   }
   renderContent() {
     const { containerStore } = this.props;
-    const { schemaData,component } = this.elementStore
-    if(component=="list"){
-      return (
-        <SchemaList containerStore={containerStore} store={this.elementStore}>
-          {this.renderChildren()}
-        </SchemaList>
-      );
+    const { component } = this.elementStore
+    if(isTaroUI(component)){
+      return <SchemaTaroUI containerStore={containerStore} store={this.elementStore}>
+      </SchemaTaroUI>
     }
-    if(component=="listItem"){
-      return (
-        <SchemaListItem containerStore={containerStore} store={this.elementStore}>
-          {this.renderChildren()}
-        </SchemaListItem>
-      );
-    }
+    // if(component=="listItem"){
+    //   return (
+    //     <SchemaListItem containerStore={containerStore} store={this.elementStore}>
+    //       {this.renderChildren()}
+    //     </SchemaListItem>
+    //   );
+    // }
     return (
       <View>
         <Text>Error: UnSupport Component!</Text>
