91d7939ec47b16f076f1b273af6f3324cf515a7f
Ciyin Huang <ciyinhuang@gmail.com>
2020-04-28 10:26:48 +0800

    up

--- src/schema-ui/common/stores/FormItemStore.tsx
+++ src/schema-ui/common/stores/FormItemStore.tsx
@@ -55,10 +55,11 @@ export class FormItemStore<IProps, IState> extends BaseElementStore<IProps,IStat
 
   @action.bound
   reset() {
+    const defaultValue = this.schema.default
     this.componentState = {} as IState;
-    this.setValue(this.defaultValue);
+    this.setValue(defaultValue);
     this.setIsValueUpdated(false);
-    this.tempValue = this.defaultValue;
+    this.tempValue = defaultValue;
   }
 
   @action.bound
