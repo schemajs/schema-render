02edf3abf6ff0ee40ecce77c7b5c4168db59bdeb
Ciyin Huang <ciyinhuang@gmail.com>
2020-04-29 18:58:59 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,8 +15,8 @@ module.exports = {
     }
   },
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'icon', href: `/schema-render/logo.png` }],
+    ['link', { rel: 'manifest', href: '/schema-render/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
